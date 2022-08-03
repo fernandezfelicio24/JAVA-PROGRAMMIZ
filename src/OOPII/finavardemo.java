7fa192c015f1c0500654bb09e1043c61273cc31c
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-08-04 06:39:02 +0700

    Faktorial and Final key word

--- src/OOPII/finavardemo.java
+++ src/OOPII/finavardemo.java
@@ -1,4 +1,43 @@
 package OOPII;
 
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
+/*
+In Java, the final keyword is used to denote constants. It can be used with variables, methods, and classes.
+
+Once any entity (variable, method or class) is declared final, it can be assigned only once. That is,
+
+the final variable cannot be reinitialized with another value
+the final method cannot be overridden
+the final class cannot be extended
+ */
 public class finavardemo {
+
+    class finalDemo {
+        //create a final method
+        public final void display() {
+            System.out.println("This is a final method.");
+        }
+    }
+
+//    class Main extends finalDemo{
+//        //try to override final method
+//        public final void display() {
+//            System.out.println("This is a final method is overriden");
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+        //create a final variable
+        final int AGE = 32;
+
+        //try to change the final variable
+        // AGE = 45;
+
+        System.out.println("Age : " + AGE);
+
+        Main obj = new Main();
+        obj.display();
+    }
 }
