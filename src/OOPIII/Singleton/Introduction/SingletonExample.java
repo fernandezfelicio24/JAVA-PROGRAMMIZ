960296d2d86808d7316f40dc2c1be5bf9fc929e6
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-08-31 15:26:07 +0700

    Singeleton Introduction

---
+++ src/OOPIII/Singleton/Introduction/SingletonExample.java
@@ -0,0 +1,41 @@
+package OOPIII.Singleton.Introduction;
+
+/*
+Java Singleton Class
+In this tutorial, we will learn to implement Singleton Design Pattern in Java with the help of examples.
+
+In Java, Singleton is a design pattern that ensures that a class can only have one object.
+
+To create a singleton class, a class must implement the following properties:
+
+Create a private constructor of the class to restrict object creation outside of the class.
+Create a private attribute of the class type that refers to the single object.
+Create a public static method that allows us to create and access the object we created.
+ Inside the method, we will create a condition that restricts us from creating more than one object.
+
+
+ */
+public class SingletonExample {
+    // private field that refers to the object
+    private static SingletonExample singleObject;
+
+    private SingletonExample() {
+        // constructor of the SingletonExample class
+    }
+
+    //public static SingletonExample getInstance() {
+        // write code that allows us to create only one object
+        // access the object as per our need
+    //}
+
+}
+
+/*
+
+In the above example,
+
+private static SingletonExample singleObject - a reference to the object of the class.
+private SingletonExample() - a private constructor that restricts creating objects outside of the class.
+public static SingletonExample getInstance() - this method returns the reference to the only object of the class.
+Since the method static, it can be accessed using the class name.
+ */
